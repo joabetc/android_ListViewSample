35be8d16febac24c1d32fa8a6dc0cbf53c4bfa19
retina <retina@Joabe.local>
2019-02-25 11:10:26 -0300

    Implementing adpater

--- app/src/main/java/com/example/listviewsample/LanguageAdapter.java
+++ app/src/main/java/com/example/listviewsample/LanguageAdapter.java
@@ -1,18 +1,32 @@
 package com.example.listviewsample;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class LanguageAdapter extends BaseAdapter {
+
+    private List<Language> languages;
+    private final Activity act;
+
+    public LanguageAdapter(List<Language> languages, Activity act) {
+        this.languages = languages;
+        this.act = act;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return languages.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return languages.get(position);
     }
 
     @Override
@@ -22,6 +36,15 @@ public class LanguageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View view = act.getLayoutInflater().inflate(R.layout.item, parent, false);
+
+        Language language = languages.get(position);
+        TextView name = view.findViewById(R.id.txtViewName);
+        ImageView logo = view.findViewById(R.id.imgViewLogo);
+
+        name.setText(language.getName());
+        logo.setImageResource(language.getLogo());
+
+        return view;
     }
 }

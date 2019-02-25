6c75f6315ac2758acc6696986700cf10e682cf01
retina <retina@Joabe.local>
2019-02-25 10:57:59 -0300

    Adjusting MainActivity

--- app/src/main/java/com/example/listviewsample/MainActivity.java
+++ app/src/main/java/com/example/listviewsample/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.listviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lstViewLanguages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstViewLanguages = findViewById(R.id.lstViewLanguages);
+        List<Language> languageList = loadLanguages();
+    }
+
+    public List<Language> loadLanguages() {
+        List<Language> list = new ArrayList<>();
+
+        Language language = new Language("C++", R.drawable.c_plus_plus);
+        list.add(language);
+        language = new Language("MySQL", R.drawable.mysql);
+        list.add(language);
+        language = new Language("jQuery", R.drawable.jquery);
+        list.add(language);
+
+        return list;
     }
 }
